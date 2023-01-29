7fb59bff27eba4b2e6ccafc3d7bf37684cb2bb89
pedrolauro15 <pedrolauro15@gmail.com>
2023-01-29 12:34:12 -0300

    Update Types

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { AudioStreamOptions } from "./types/options";
 const { Recording } = NativeModules;
 const eventEmitter = new NativeEventEmitter(Recording);
 
-interface ExportData {
+interface RnLiveAudio {
   init: (options: AudioStreamOptions) => void;
   start: () => void;
   stop: () => void;
-  addRecordingEventListener: (listener: (data: any) => void) => EmitterSubscription;
+  addRecordingEventListener: (listener: (data: ArrayBuffer) => void) => EmitterSubscription;
 }
 export default {
   init: (options: AudioStreamOptions) => Recording.init(options),
   start: () => Recording.start(),
   stop: () => Recording.stop(),
-  addRecordingEventListener: (listener: (data: any) => void) =>
+  addRecordingEventListener: (listener: (data: ArrayBuffer) => void) =>
     eventEmitter.addListener("recording", listener),
-} as ExportData;
+} as RnLiveAudio;
